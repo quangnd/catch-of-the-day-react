77d292428e1f125912c5513e11e75a149b39f4d8
quangnd <quangnd.edu@gmail.com>
2018-02-19 22:59:20 +0700

    Add transitionTo route

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e) }>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store name" defaultValue={getFunName()}/>
+        <input type="text" required placeholder="Store name" defaultValue={getFunName()} ref={(input) => this.storeInput = input}/>
         <button type="submit">Visit store</button>
       </form>
     );
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
