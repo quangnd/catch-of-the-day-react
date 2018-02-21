c75541bb3e8ee77eaa85943125c0edf359a18187
quangnd <quangnd.edu@gmail.com>
2018-02-21 21:54:42 +0700

    Load samples with one click

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
@@ -13,6 +14,7 @@ class App extends React.Component {
     }
 
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
   }
 
   addFish(fish) {
@@ -24,6 +26,10 @@ class App extends React.Component {
     })
   }
 
+  loadSamples() {
+    this.setState({ fishes: sampleFishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -31,7 +37,7 @@ class App extends React.Component {
           <Header tagline="Fresh seafood market"/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
