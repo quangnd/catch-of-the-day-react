5825bef86878f64512cb06c440c407809c4f944c
quangnd <quangnd.edu@gmail.com>
2018-02-23 08:02:46 +0700

    Persisting fish state in Firebase

---
+++ src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDQLmJcq8Nu-rDCeJFyX85FeUlIAJavFE4",
+  authDomain: "mun-fish-market.firebaseapp.com",
+  databaseURL: "https://mun-fish-market.firebaseio.com",
+  projectId: "mun-fish-market"
+});
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
@@ -19,6 +20,17 @@ class App extends React.Component {
     this.addToOrder = this.addToOrder.bind(this);
   }
 
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+  
   addFish(fish) {
     const fishes = {...this.state.fishes }; // clone and do not change original state
     const timestamp = Date.now();
